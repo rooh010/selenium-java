aa5d84286ca0d59937af1622b17a374edf4c2d03
Andrew Fay <Andrew.fay@hotmail.co.uk>
2021-10-23 17:55:58 +0100

    added quick webdriver test

--- src/main/java/Implementation.java
+++ src/main/java/Implementation.java
@@ -1,19 +1,21 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.Test;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.CoreMatchers.containsString;
+public class Implementation {
+    public static void main(String[] args) {
 
-public class TestImplementation {
-
-    private WebDriver driver;
-    @Test
-    public void verifyGitHubTitle() {
         System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+
+        System.out.println(System.getProperty("user.dir"));
+
+        System.out.println(System.getProperty("webdriver.chrome.driver"));
+
         WebDriver driver;
+
         driver = new ChromeDriver();
+
         driver.get("https://www.github.com");
-        assertThat(driver.getTitle(), containsString("GitHub"));
+        driver.quit();
+
     }
 }
