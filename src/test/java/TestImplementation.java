cff621cb605f07d418326f7900c1b2341ff01f0b
Andrew Fay <Andrew.fay@hotmail.co.uk>
2021-10-23 16:58:04 +0100

    added files

--- src/test/java/TestImplementation.java
+++ src/test/java/TestImplementation.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class TestImplementation {
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestImplementation {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupWebdriverChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/main/resources/chromedriver");
+    }
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("https://www.github.com");
+        assertThat(driver.getTitle(), containsString("GitHub"));
+    }
 }
